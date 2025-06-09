d15d02ff29ff1b69a0b18cce8cadf9b156f65dd3
iseukomayatuma <kmm2459305@stu.o-hara.ac.jp>
2025-06-09 13:54:29 +0900

    キッカー狂暴化時のステータス作成
    新規スクリプト : アタッチオブジェクト
    KickerStatBlock : アタッチしてない
    EnmeyState : アタッチしてない
    MoveDir : アタッチしてない
    変更スクリプト ------------------
    KickerMoveCommnader
    KickerStatus

--- The_Climb/Assets/Script/Enemy/KickerStatus.cs
+++ The_Climb/Assets/Script/Enemy/KickerStatus.cs
@@ -1,14 +1,57 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "KickerStatus", menuName = "GameDate/Enemy/KickerStatus")]
-////  キッカーのステータス
+//  キッカーのステータス
 public class KickerStatus : ScriptableObject
 {
+    //  状態とステータスをもつクラス
+    [System.Serializable]
+    public class StateStatPair
+    {
+        public EnemyStates State;    //  敵の状態(通常時と狂暴化)
+        public KickerStatBlock Stats;    //  ステータスを持つクラス
+    }
+
     [Header("Kicker Status")]
-    [SerializeField] float MoveSpd;    //  移動速度
-    [SerializeField] float JumpForce;    //  ジャンプ力
-    [SerializeField] float JumpFrequency;  //  ジャンプする間隔
-    public float MoveSpdProperty => MoveSpd;
-    public float JumpForceProperty => JumpForce;
-    public float JumpFrequencyProperty => JumpFrequency;
+    public List<StateStatPair> StateStats = new();    //  状態とステータスを持つクラスのリスト(データ入力用)
+
+    Dictionary<EnemyStates, KickerStatBlock> StatMap;    //  状態とステータスの辞書(処理用)
+
+    void OnEnable()
+    {
+        //  スーテータスマップの初期化
+        BuildStatMap();
+    }
+    //  ステータスマップ初期化
+    void BuildStatMap()
+    {
+        StatMap = new();
+        foreach (var pair in StateStats)
+        {
+            if (!StatMap.ContainsKey(pair.State))
+            {
+                StatMap.Add(pair.State, pair.Stats);
+            }
+        }
+    }
+    //  状態に応じたステータスの取得
+    public KickerStatBlock GetStats(EnemyStates State)
+    {
+        return StatMap.TryGetValue(State, out KickerStatBlock stats) ? stats : null;
+    }
 }
+//  以下コード保存所
+
+//[Header("Normal Time")]
+//[SerializeField] float MoveSpd_Normal;    //  移動速度(平常時)
+//[SerializeField] float JumpForce_Normal;    //  ジャンプ力(平常時)
+//[SerializeField] float JumpFrequency_Normal;  //  ジャンプする間隔(平常時)
+//[Header("Violent Time")]
+//[SerializeField] float MoveSpd;    //  移動速度(狂暴化時)
+//[SerializeField] float JumpForce;    //  ジャンプ力(狂暴化時)
+//[SerializeField] float JumpFrequency;  //  ジャンプする間隔(狂暴化時)
+
+//public float MoveSpdProperty => MoveSpd_Normal;
+//public float JumpForceProperty => JumpForce_Normal;
+//public float JumpFrequencyProperty => JumpFrequency_Normal;
