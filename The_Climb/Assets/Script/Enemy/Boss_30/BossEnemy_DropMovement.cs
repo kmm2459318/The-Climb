881d2cd4bd6a41f7b2a3c93dd9351d3305b43379
kmm2459318 <kmm2459318@stu.o-hara.ac.jp>
2025-06-19 11:56:31 +0900

    Squashed commit of the following:
    commit 7cb33c684496e1dc12fe44fabb97cb1ac54538d6
    Merge: dadb8a6 c04f36e
    Author: Sugi <kmm2459318@stu.o-hara.ac.jp>
    Date:   Thu Jun 19 11:56:14 2025 +0900
    
        Merge pull request #36 from kmm2459318/αKitano-Hayato
    
        30階層のボスの挙動管理スクリプト
    
    commit c04f36e2067b4d81e4169a541b3c94e1ae83e649
    Merge: 11b2a7c dadb8a6
    Author: kmm2459318 <kmm2459318@stu.o-hara.ac.jp>
    Date:   Thu Jun 19 11:27:53 2025 +0900
    
        Merge branch 'main' into αKitano-Hayato
    
    commit 11b2a7c4c5e6667d64194860ac111e5063931093
    Merge: e30e5f7 bc8999f
    Author: kmm2459318 <kmm2459318@stu.o-hara.ac.jp>
    Date:   Thu Jun 19 11:25:22 2025 +0900
    
        Merge branch 'main' into αKitano-Hayato
    
    commit dadb8a6afcbb545d3f40d1c4e050baf844ed99b0
    Merge: bc8999f 99cbfae
    Author: Sugi <kmm2459318@stu.o-hara.ac.jp>
    Date:   Thu Jun 19 11:24:50 2025 +0900
    
        Merge pull request #35 from kmm2459318/αMatuyama-Kousei
    
        αmatuyama kousei
    
    commit 99cbfae8ac2094d7eda93e26b916ef2995b26ec8
    Merge: 01bcc26 bc8999f
    Author: kmm2459318 <kmm2459318@stu.o-hara.ac.jp>
    Date:   Thu Jun 19 11:24:00 2025 +0900
    
        Merge branch 'main' into αMatuyama-Kousei
    
    commit e30e5f715240196aefb08169114dcdd0c86560fd
    Merge: c332bfa c0afe82
    Author: kmm-2459386 <kmm2459386@stu.o-hara.ac.jp>
    Date:   Wed Jun 18 15:26:14 2025 +0900
    
        Merge branch 'main' into αKitano-Hayato
    
    commit c332bfaf9eb17a541210c5c75f7c4dec67f5062e
    Author: kmm-2459386 <kmm2459386@stu.o-hara.ac.jp>
    Date:   Wed Jun 18 15:25:43 2025 +0900
    
        30階層のボスの挙動管理スクリプト
    
        画面外から対角線上に突進を繰り返す。
        突進を繰り返した後、一回目だけ最初から上を陣取り
        メテオドロップを行う。そのあとは、ゆっくりと上昇し
        再度メテオドロップを行う。なお、これらの数値をステータスで管理している。
        Use Gravityを切っておかないとバグります。ここもいずれ修正できたらしたい
    
    commit 01bcc268cf865ea3658d1a13786e864d7188bc54
    Merge: 4bf66fc c0afe82
    Author: iseukomayatuma <kmm2459305@stu.o-hara.ac.jp>
    Date:   Wed Jun 18 14:33:45 2025 +0900
    
        Merge branch 'main' into αMatuyama-Kousei
    
    commit 4bf66fca5e1075bb5fe60a883aa2dc200f120301
    Merge: 7a6bf90 6d36ee8
    Author: iseukomayatuma <kmm2459305@stu.o-hara.ac.jp>
    Date:   Wed Jun 18 14:33:14 2025 +0900
    
        Merge branch 'αMatuyama-Kousei' of https://github.com/kmm2459318/The-Climb into αMatuyama-Kousei
    
    commit 7a6bf908f34eaf4d12c829ffeab5ddb2cca0637c
    Author: iseukomayatuma <kmm2459305@stu.o-hara.ac.jp>
    Date:   Wed Jun 18 14:33:11 2025 +0900
    
        闇が四隅から進行してくるシェーダーを作成
    
        新規Script : アタッチオブジェクト
        DarkContoroller : EdgeDarkContoroller
        FloorType : アタッチなし
        EdgeDarkStatus : アタッチなし
        EdgeDarkEffectFeature : アタッチなし
        EdgeDarkStatBlock : アタッチなし
        EdgeDarkDefusion : Dark.mat
        From_Teacher : Dark2.mat
        変更Script------------------
        KickerStatus
        kickerStateBlock
        KickerMovementCommander
        その他------------------
        Darkマテリアル追加
        Dark2マテリアル追加

--- The_Climb/Assets/Script/Enemy/Boss_30/BossEnemy_DropMovement.cs
+++ The_Climb/Assets/Script/Enemy/Boss_30/BossEnemy_DropMovement.cs
@@ -6,6 +6,7 @@ public class BossEnemy_DropMovement : MonoBehaviour
 {
     public DropStats stats;
     private Rigidbody rb;
+    [SerializeField] private Transform player;
 
     private enum State
     {
@@ -24,9 +25,9 @@ public class BossEnemy_DropMovement : MonoBehaviour
     private bool firstHoverDone = false;
     private int aimMoveCounter = 0;
     private bool aimingToRight = true;
+    private bool movingToB = true;
 
-    private bool movingToB = true; // true = A→B, false = B→A
-
+    private bool hasDropped = false;
 
     private void Awake()
     {
@@ -56,8 +57,9 @@ public class BossEnemy_DropMovement : MonoBehaviour
                 break;
 
             case State.Waiting:
-                // 待機中（コルーチン）
+                // コルーチン中の待機
                 break;
+
             case State.Aiming:
                 PerformAiming();
                 break;
@@ -89,7 +91,7 @@ public class BossEnemy_DropMovement : MonoBehaviour
             }
             else
             {
-                movingToB = !movingToB; // 方向反転して再開
+                movingToB = !movingToB;
             }
         }
     }
@@ -99,7 +101,6 @@ public class BossEnemy_DropMovement : MonoBehaviour
         currentState = State.Waiting;
         firstHoverDone = true;
 
-        // Y方向に上昇させてから停止
         transform.position = new Vector3(transform.position.x, stats.hoverHeight, 0f);
         yield return new WaitForSeconds(stats.waitBeforeMeteor);
 
@@ -108,46 +109,6 @@ public class BossEnemy_DropMovement : MonoBehaviour
         currentState = State.Aiming;
     }
 
-    void PerformMeteorDrop()
-    {
-        rb.linearVelocity = new Vector3(0f, -stats.meteorDropSpeed, 0f);
-
-        if (transform.position.y <= stats.groundY)
-        {
-            meteorCounter++;
-            rb.linearVelocity = Vector3.zero;
-
-            if (meteorCounter >= stats.meteorDropCount)
-            {
-                rushCounter = 0;
-                movingToB = true; // Aに戻ってまた往復
-                transform.position = new Vector3(stats.pointA_X, stats.groundY, 0f);
-                currentState = State.RushMove;
-            }
-            else
-            {
-                StartCoroutine(WaitAndRise());
-            }
-        }
-    }
-
-    IEnumerator WaitAndRise()
-    {
-        currentState = State.Waiting;
-        yield return new WaitForSeconds(stats.waitBeforeMeteor);
-        currentState = State.Rising;
-    }
-
-    void PerformRising()
-    {
-        rb.linearVelocity = new Vector3(0f, stats.riseSpeed, 0f);
-
-        if (transform.position.y >= stats.hoverHeight)
-        {
-            rb.linearVelocity = Vector3.zero;
-            currentState = State.MeteorDrop;
-        }
-    }
     void PerformAiming()
     {
         float targetX = aimingToRight ? stats.aimMoveRightX : stats.aimMoveLeftX;
@@ -169,12 +130,60 @@ public class BossEnemy_DropMovement : MonoBehaviour
             }
         }
     }
+
     IEnumerator WaitAndDrop()
     {
         currentState = State.Waiting;
         yield return new WaitForSeconds(stats.waitBeforeMeteor);
         currentState = State.MeteorDrop;
+        hasDropped = false; // Drop前にリセット
     }
 
+    void PerformMeteorDrop()
+    {
+        if (hasDropped) return;
 
+        hasDropped = true;
+        Vector3 dir = (player.position - transform.position).normalized;
+        rb.linearVelocity = dir * stats.meteorDropSpeed;
+    }
+
+    void PerformRising()
+    {
+        rb.linearVelocity = new Vector3(0f, stats.riseSpeed, 0f);
+            
+        if (transform.position.y >= stats.hoverHeight)
+        {
+            rb.linearVelocity = Vector3.zero;
+            currentState = State.MeteorDrop;
+            hasDropped = false; // Rising経由のMeteorDropでも一度だけ落下
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (currentState == State.MeteorDrop && collision.gameObject.CompareTag("Ground"))
+        {
+            rb.linearVelocity = Vector3.zero;
+            hasDropped = false;
+            meteorCounter++;
+
+            if (meteorCounter >= stats.meteorDropCount)
+            {
+                rushCounter = 0;
+                currentState = State.RushMove;
+            }
+            else
+            {
+                StartCoroutine(WaitAndRise());
+            }
+        }
+    }
+
+    IEnumerator WaitAndRise()
+    {
+        currentState = State.Waiting;
+        yield return new WaitForSeconds(stats.waitBeforeMeteor);
+        currentState = State.Rising;
+    }
 }
