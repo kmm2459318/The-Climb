099eced83b129ac4c8a4b17f430f9ab631514695
kmm2459358 <kmm2459358@stu.o-hara.ac.jp>
2025-06-26 10:32:00 +0900

    攻撃判定、ブロック崩し
    変更：TagName、PlayerAttack、PlayerSpecialAction
    
    追加：タグ「Enemy」
    
    追加機能：敵への攻撃、ブロック破壊

--- The_Climb/Assets/Script/Constant/TagName.cs
+++ The_Climb/Assets/Script/Constant/TagName.cs
@@ -3,6 +3,8 @@ public readonly struct TagName
 {
     public static readonly TagName Wall = new TagName("Wall");    //  壁のタグの名前
     public static readonly TagName Ground = new TagName("Ground");    //  地面のタグの名前
+    public static readonly TagName Enemy = new TagName("Enemy");    //  敵のタグの名前
+    public static readonly TagName Player = new TagName("Player");    //  プレイヤーのタグの名前
     public string Value { get; }
 
     //  Valueにvalueをセットするコンストラクタ
