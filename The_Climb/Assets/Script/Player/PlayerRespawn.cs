70129971989143dc0e3931ddd699b2bee5dfdd0e
kmm2459358 <kmm2459358@stu.o-hara.ac.jp>
2025-09-02 12:41:34 +0900

    リスポーンするようにはなったが重大バグあり
    PleyerRespawn：プレイヤーのy座標が一定数を下回ったら最後のリスポーン地点にワープするようにした。
    
    新規スクリプト
    RespawnGroundCheck：プレイヤーの足元左右にリスポーン位置として最適かの判定用スクリプト
    
    新規オブジェクト
    RespawnGroundCehckLeft ＆ RespawnGroundCehckRight：プレイヤーの足元左右にリスポーン位置として最適かの判定用コリダー用

--- The_Climb/Assets/Script/Player/PlayerRespawn.cs
+++ The_Climb/Assets/Script/Player/PlayerRespawn.cs
@@ -1,23 +1,37 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class PlayerRespawn : MonoBehaviour
 {
+    private RespawnGroundCheck respawnGroundCheckLeft;
+    private RespawnGroundCheck respawnGroundCheckRight;
+
+    private bool leftGroundCheck = false; //プレイヤーの左のリスポーン地面判定
+    private bool rightGroundCheck = false; //プレイヤーの右のリスポーン地面判定
+    private Vector3 lastSavePos; //リスポーン位置
+
     void Start()
     {
-        
+        respawnGroundCheckLeft = GameObject.Find("RespawnGroundCehckLeft").GetComponent<RespawnGroundCheck>();
+        respawnGroundCheckRight = GameObject.Find("RespawnGroundCehckRight").GetComponent<RespawnGroundCheck>();
+        lastSavePos = gameObject.transform.position;
     }
 
     void Update()
     {
-        
-    }
+        //リスポーンする地面のチェックを随時更新
+        leftGroundCheck = respawnGroundCheckLeft.isRespawnGrounded;
+        rightGroundCheck = respawnGroundCheckRight.isRespawnGrounded;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag(""))
+        //どっちもtrueの時リスポーン地点を随時更新
+        if (leftGroundCheck && rightGroundCheck)
         {
-            SceneManager.LoadScene("");
+            lastSavePos = transform.position + new Vector3(0, 0.1f, 0);
         }
+
+        //落ちた判定(今は簡易)
+        if (transform.position.y < -4.3f)
+        {
+            transform.position = lastSavePos;
+        } 
     }
 }
