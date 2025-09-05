6477a4deb608e123df7a3646dbc12dc4b08e5a9c
iseukomayatuma <kmm2459305@stu.o-hara.ac.jp>
2025-09-05 09:42:07 +0900

    昼夜 : 時間経過のロジック修正 & チェイサーのアセット導入
    新規Script : アタッチオブジェクト
    ChaiserMoveCommander : 現時点ではアタッチなし
    SafeEventUtility : サブスクライブUtility
    変更Script : 変更内容
    KickerMoveCommander : 時間経過のロジック変更による狂暴化のコード修正
    ITimeProvider : サブスクUtilityができたことによるサブスク変数を削除
    TimeManager : サブスクUtilityができたことによる狂暴化の仕組み変更
    追加機能------------------
    サブスクのUtilityを追加
    確認方法-----------------
    敵がいない状態でゲームが始まってもTimeManagerがエラーを起こさない

---
+++ The_Climb/Assets/Script/Enemy/Movement/ChaiserMoveCommnader.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace TheClimb.Enemy
+{
+    public class ChaiserMoveCommnader : MonoBehaviour
+    {
+
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
+////  memo
+///@update‚Å‰ñ‚µ‘±‚¯‚Ä—â‚Á‚Æ‚·‚é(”ò‚Ñ‰z‚¦‚é)
+///  ƒmƒŠƒmƒŠ‚Å‚¤‚Ü‚­‚©‚í‚¹‚é
