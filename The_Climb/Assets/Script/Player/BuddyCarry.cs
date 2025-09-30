7e7cc7916abc5a04e3922afa2eb6032845473c64
kmm2459358 <kmm2459358@stu.o-hara.ac.jp>
2025-09-30 09:39:01 +0900

    バディのおんぶ
    新規タグ：Buddy
    
    新規オブジェクト：Buddy
    
    新規スクリプト
    
    BuddyCarry：バディをおんぶしたり、降ろしたりする
    
    PlayerAttackNakamura：自分用コピー、タグBuddyでy座標リセットが反応しないように
    
    新規Prefab
    
    PlayerNakamura：Playerの自分用コピー

---
+++ The_Climb/Assets/Script/Player/BuddyCarry.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class BuddyCarry : MonoBehaviour
+{
+    private GameObject buddy;             //Buddyのゲームオブジェクト
+    private PositionConstraint buddyPos;  //BuddyのPositionConstraint（おんぶに使ってる追従のコンポーネント）
+    PlayerState state;
+
+    private bool carryingBuddy = true;    //Buddyをおんぶしてる状態か判定
+    private bool nearBuddy = false;       //Buddyが近くにいるか判定
+
+    void Start()
+    {
+        buddy = GameObject.Find("Buddy");
+        buddyPos = buddy.GetComponent<PositionConstraint>();
+        state = GetComponent<PlayerState>();
+    }
+
+    void Update()
+    {
+        //向いてる方向によっておんぶしてるバディの場所を調整
+        if (buddyPos != null && state.playerDirectionRight)
+        {
+            buddyPos.translationOffset = new Vector3(-0.4f, 1f, 0f);
+        }
+        else
+        {
+            buddyPos.translationOffset = new Vector3(0.4f, 1f, 0f);
+        }
+
+        //Carryボタン（仮）
+        if (Input.GetKeyDown(KeyCode.C) && state.isGrounded)
+        {
+            if (carryingBuddy)  //おんぶしてる場合、バディを降ろす
+            {
+                carryingBuddy = false;
+                buddyPos.constraintActive = false;
+                buddy.transform.position = transform.position + Vector3.up * 0.5f;
+            }
+            else if (nearBuddy)  //おんぶしてない場合、バディをおんぶする
+            {
+                carryingBuddy = true;
+                buddyPos.constraintActive = true;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Buddy"))
+        {
+            nearBuddy = true;  //Buddyが近くにいる
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Buddy"))
+        {
+            nearBuddy = false;  //Buddyが近くにいない
+        }
+    }
+}
