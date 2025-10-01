a4da528a79c9c6f4ca8166966460aef436dfa6b3
kmm-2459382 <kmm2459382@stu.o-hara.ac.jp>
2025-10-01 15:18:04 +0900

    過去と現代の切り替え
    遷移したときに壁が次のステージであると動けなくなる

--- The_Climb/Assets/Script/System/Time/TimeChange.cs
+++ The_Climb/Assets/Script/System/Time/TimeChange.cs
@@ -1,25 +1,31 @@
 using UnityEngine;
 
-
 public class TimeChange : MonoBehaviour
 {
     [Header("ステージセレクト")]
-    [SerializeField] private GameObject[] MapPrefabs; // プレハブ登録用
-    private GameObject[] MapInstance;   // 生成したマップの参照
+    [SerializeField] private GameObject[] MapPrefabs;
+    private GameObject[] MapInstance;
+
     [Header("プレイヤーの参照")]
     [SerializeField] private Transform Player;
-    [Tooltip("KeyBindのスクリプト")]
+
     public KeyBind KeyBind;
 
-    public int[] CurrentMapIndex = { 0, 1 };
-    private int CurrentActiveIndex = 0;
+    [Header("フェード制御")]
+    [SerializeField] private ScreenFader fader;
 
+    [Header("クールダウン時間(秒)")]
+    [SerializeField] private float switchCooldown = 2f;
+
+    private int CurrentActiveIndex = 0;
+    private SafeSpawner spawner;
 
 
     void Start()
     {
         KeyBind = GameObject.Find("KeyManager").GetComponent<KeyBind>();
-        // マップを全部生成して非表示にする
+        spawner = Player.GetComponent<SafeSpawner>();
+
         MapInstance = new GameObject[MapPrefabs.Length];
         for (int i = 0; i < MapPrefabs.Length; i++)
         {
@@ -27,39 +33,32 @@ public class TimeChange : MonoBehaviour
             MapInstance[i].SetActive(false);
         }
 
-        // 最初のマップだけ有効化
         MapInstance[CurrentActiveIndex].SetActive(true);
     }
+
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyBind.timeSwitch))
+        if (!fader.IsFading && Input.GetKeyDown(KeyBind.timeSwitch))
         {
-            Debug.Log("変わりました");
-            SwitchToNextMap();
+            fader.FadeAndDo(SwitchToNextMap);
         }
     }
 
-    public void SwitchToNextMap()
+    private void SwitchToNextMap()
     {
-        // 現在のマップを非表示
         MapInstance[CurrentActiveIndex].SetActive(false);
 
-        // 次のマップに進める
         CurrentActiveIndex++;
         if (CurrentActiveIndex >= MapInstance.Length)
-        {
             CurrentActiveIndex = 0;
-        }
 
-        // 新しいマップを表示
         MapInstance[CurrentActiveIndex].SetActive(true);
+
+        // 安全な位置に修正
+        if (spawner != null)
+        {
+            Player.position = spawner.FindSafePosition(Player.position);
+        }
     }
 }
-
-
-  
-
-
-
-
-
