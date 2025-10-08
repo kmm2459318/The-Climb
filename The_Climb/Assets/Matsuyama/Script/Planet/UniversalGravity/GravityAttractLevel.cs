c377ccf29085ea7e5b40b341a3d18664f9a31154
kmm-2459393 <kmm2459305@stu.o-hara.ac.jp>
2025-10-08 11:33:26 +0900

    天体:万有引力:範囲の作成 & 引き寄せ
    新規Script : アタッチオブジェクト
    AttractVectleCalculate : アタッチなし
    GravitationManager : アタッチなし
    GravitationTargetAttracter : 天体
    GravitationTargetID : アタッチなし::
    GravitationTargetStatusHolder : アタッチなし
    GravityAttractLevel : アタッチなし
    IGravitationTarget : アタッチなし
    GravitationTargetRegistry : アタッチなし
    GravitationTargetEntry : アタッチなし
    GravutationTargetRegister : 万有引力操作対象
    GravitationTargetStatusBlock : アタッチなし
    変更Script : 変更内容
    PlanetStatusBlock : 新項目を追加
    追加機能------------------
    天体の万有引力影響範囲内にあるオブジェクトを引き寄せる

---
+++ The_Climb/Assets/Matsuyama/Script/Planet/UniversalGravity/GravityAttractLevel.cs
@@ -0,0 +1,11 @@
+namespace TheClimb.UniversalGravity
+{
+    public enum GravitationLevel    //  –œ—Lˆø—ÍƒŒƒxƒ‹
+    {
+        None,        //  ‚È‚µ
+        Weak,        //  ã‚¢
+        Normal,      //  •’Ê
+        Strong,      //  ‹­‚¢
+        TooStrong    //  ‹­‚·‚¬‚é
+    }
+}
