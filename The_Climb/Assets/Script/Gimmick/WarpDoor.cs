17d75438a245c021be7ed8921b43e9b58d6e1ed7
2459311 <kmm2459358@stu.o-hara.ac.jp>
2025-11-07 12:01:06 +0900

    ステージの触り部分、ドアの触り部分
    PlayerAttackNakamura：頭ぶつかったときのÝ座標リセットの条件修正
    BuddyCarry：最初から相棒おんぶしてない状態に
    
    新規スクリプト
    WarpDoor：移動ドア

---
+++ The_Climb/Assets/Script/Gimmick/WarpDoor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class WarpDoor : MonoBehaviour
+{
+    [SerializeField] private bool canGoBack = false;
+    [Header("canGoBackがtrueであればgoToDoorを" +
+        "falseならgoToWhereを指定しろ")]
+    [SerializeField] private GameObject goToDoor;
+    [SerializeField] private GameObject goToWhere;
+
+    void Start()
+    {
+        if ((canGoBack && goToDoor != null) || (!canGoBack && goToWhere != null))
+        {
+            Debug.LogError(canGoBack ? "goToDoor" : "goToWhere" + "を指定してください。");
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
