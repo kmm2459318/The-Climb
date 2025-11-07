2d34f6eb491bd70abd963bb6e377827425efc8d2
kmm2459378 <kmm2459378@stu.o-hara.ac.jp>
2025-11-07 16:07:01 +0900

    あ

--- The_Climb/Assets/Script/Player/Gun/StickRotate.cs
+++ The_Climb/Assets/Script/Player/Gun/StickRotate.cs
@@ -1,20 +1,20 @@
-﻿using UnityEngine;
+﻿//using UnityEngine;
 
-public class StickRotate : MonoBehaviour
-{
-    void Update()
-    {
-        if (Input.GetMouseButton(0))
-        {
-            //Debug.Log("クリックされました");
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            worldMouse.z = 0;
+//public class StickRotate : MonoBehaviour
+//{
+//    void Update()
+//    {
+//        if (Input.GetMouseButton(0))
+//        {
+//            //Debug.Log("クリックされました");
+//            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+//            worldMouse.z = 0;
 
-            Vector3 direction = mousePos - transform.position;
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+//            Vector3 direction = mousePos - transform.position;
+//            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
             
-            Debug.Log(Quaternion.Euler(0,0, angle));
-            transform.rotation = Quaternion.Euler(0, 0, angle);
-        }
-    }
-}
+//            Debug.Log(Quaternion.Euler(0,0, angle));
+//            transform.rotation = Quaternion.Euler(0, 0, angle);
+//        }
+//    }
+//}
