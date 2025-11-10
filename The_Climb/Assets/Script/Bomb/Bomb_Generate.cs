d155ab4656962397cdd60ee17cc05f3256e1a8fc
kmm2459378 <kmm2459378@stu.o-hara.ac.jp>
2025-11-10 16:24:12 +0900

    爆弾完成

---
+++ The_Climb/Assets/Script/Bomb/Bomb_Generate.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Bomb_Generate : MonoBehaviour
+{
+    [SerializeField] GameObject bombPrefab;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            GameObject bomb = Instantiate(bombPrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
---
+++ The_Climb/Assets/Script/Bomb/Player_Bomb.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class Player_Bomb : MonoBehaviour
+{
+    [SerializeField] ParticleSystem b_particle;
+    [SerializeField] float b_force = 10;
+    [SerializeField] float b_radius = 5;
+    [SerializeField] float b_upward = 0;
+    [SerializeField] float b_time = 3;
+
+    private float b_explosion = 0;
+    private bool exploded = false;
+    private Vector3 b_pos;
+
+    void Update()
+    {
+        b_explosion += Time.deltaTime; 
+
+        if (b_explosion >= b_time && !exploded)
+        {
+            exploded = true;
+            Explosion();
+        }
+    }
+
+    void Explosion()
+    {
+        b_pos = transform.position;
+
+        if (b_particle != null)
+            b_particle.Play();
+
+        Collider[] hitColliders = Physics.OverlapSphere(b_pos, b_radius);
+        for (int i = 0; i < hitColliders.Length; i++)
+        {
+            var rb = hitColliders[i].GetComponent<Rigidbody>();
+            if (rb)
+            {
+                rb.AddExplosionForce(b_force, b_pos, b_radius, b_upward, ForceMode.Impulse);
+            }
+        }
+
+        Debug.Log("爆発しました");
+        Destroy(gameObject, 1f); // 1秒後に削除
+    }
+}
