9feb67aa534cf3013fed6b643d14d954844529a8
2459358 <kmm2459358@stu.o-hara.ac.jp>
2026-01-27 16:42:12 +0900

    Buddy置き去りUI、矢印UI（ボタン押した後の挙動は考慮していない）
    ArrowUIManager：ほぼ完（ボタン押した後はまだ）
    
    LightDarkWorld：BuddyUIBackgroundの色変更
    
    新規スクリプト
    BuddyUIManager：Buddyが画面外行ったときに出るUIのManager

--- The_Climb/Assets/Script/System/ArrowUIManager.cs
+++ The_Climb/Assets/Script/System/ArrowUIManager.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
+
 
 public class ArrowUIManager : MonoBehaviour
 {
     RectTransform arrowRectTransform = null;
 
-    [SerializeField] private Transform target;
+    private Transform target;
     [SerializeField] private Camera uiCamera;
     [SerializeField] private RectTransform canvasRect;
     [SerializeField] private RectTransform arrowRect;
@@ -15,15 +17,20 @@ public class ArrowUIManager : MonoBehaviour
 
     [SerializeField] private Vector2 edgeBuffer = new Vector2(50f, 50f);
 
+    [SerializeField] private List<Transform> buttons;
+
     void Start()
     {
         arrowRectTransform = GetComponent<RectTransform>();
 
         arrowRectTransform.localScale = Vector3.one;
+        target = buttons[0].transform;
     }
 
     void Update()
     {
+        //ターゲット設定
+        SetTarget();
         Vector3 screenPos = uiCamera.WorldToScreenPoint(target.position);
 
         bool isOnScreen =
@@ -31,28 +38,90 @@ public class ArrowUIManager : MonoBehaviour
         screenPos.x >= 0 && screenPos.x <= Screen.width &&
         screenPos.y >= 0 && screenPos.y <= Screen.height;
 
-        // 画面内なら非表示
+        //画面内なら非表示
         if (isOnScreen)
         {
             arrowRect.gameObject.SetActive(false);
-            return;
+            ScreenInArrow(screenPos);
         }
+        else
+        {
+            ScreenOutArrow(screenPos);
+        }
+    }
 
-        // 画面外なら表示
+    //各ボタンの距離を調べ、ターゲットを設定
+    void SetTarget()
+    {
+        float minDistance = float.MaxValue;
+        foreach (Transform btn in buttons)
+        {
+            Vector3 toTarget = btn.position - uiCamera.transform.position;
+            float distance = toTarget.magnitude;
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                target = btn;
+            }
+        }
+    }
+
+    //画面内矢印表示
+    void ScreenInArrow(Vector3 pos)
+    {
+        //画面外なら表示
         arrowRect.gameObject.SetActive(true);
 
         //Vector3 toTarget = target.position - uiCamera.transform.position;
         //float distance = toTarget.magnitude;
         //float scale = Mathf.Clamp(rateScale / distance, scaleMin, scaleMax);
 
-        // 画面端クランプ
-        screenPos.x = Mathf.Clamp(screenPos.x, edgeBuffer.x, Screen.width - edgeBuffer.x);
-        screenPos.y = Mathf.Clamp(screenPos.y, edgeBuffer.y, Screen.height - edgeBuffer.y);
+        arrowRect.rotation = Quaternion.Euler(0f, 0f, 0f);
 
+        pos.y += 190f;  //少し上にずらす
+
+        //位置変換
         Vector2 localPos;
         RectTransformUtility.ScreenPointToLocalPointInRectangle(
             canvasRect,
-            screenPos,
+            pos,
+            null,
+            out localPos
+        );
+
+        arrowRect.localPosition = localPos;
+    }
+
+    //画面外矢印表示
+    void ScreenOutArrow(Vector3 pos)
+    {
+        //画面外なら表示
+        arrowRect.gameObject.SetActive(true);
+
+        //Vector3 toTarget = target.position - uiCamera.transform.position;
+        //float distance = toTarget.magnitude;
+        //float scale = Mathf.Clamp(rateScale / distance, scaleMin, scaleMax);
+
+        //画面端クランプ
+        Vector3 screenCenter = new Vector3(Screen.width, Screen.height, 0) * 0.5f;
+        Vector3 fromCenterToTarget = (pos - screenCenter).normalized;
+
+        pos = screenCenter + fromCenterToTarget * ((Mathf.Min(Screen.width, Screen.height) * 0.5f) - edgeBuffer.magnitude);
+        pos.x = Mathf.Clamp(pos.x, edgeBuffer.x, Screen.width - edgeBuffer.x);
+        pos.y = Mathf.Clamp(pos.y, edgeBuffer.y, Screen.height - edgeBuffer.y);
+
+        //角度計算と回転
+        float angle = Mathf.Atan2(
+            pos.y - 2.5f - (Screen.height / 2),
+            pos.x - (Screen.width / 2)
+        ) * Mathf.Rad2Deg;
+        arrowRect.rotation = Quaternion.Euler(0, 0, angle + 90f);
+
+        //位置変換
+        Vector2 localPos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            canvasRect,
+            pos,
             null,
             out localPos
         );
