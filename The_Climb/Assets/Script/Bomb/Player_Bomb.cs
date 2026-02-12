c2363ad3bd83aec5ada78d8b6219b71f7d2428d6
kmm-2459386 <159798658+taiyakinosippo@users.noreply.github.com>
2026-02-12 14:33:57 +0900

    爆弾のSEを設定、爆弾が爆発すると座標がずれることの修正

--- The_Climb/Assets/Script/Bomb/Player_Bomb.cs
+++ The_Climb/Assets/Script/Bomb/Player_Bomb.cs
@@ -8,6 +8,8 @@ public class Player_Bomb : MonoBehaviour
     [SerializeField] float b_radius = 5;
     [SerializeField] float b_upward = 0;
     [SerializeField] float b_time = 3;
+    [SerializeField] AudioClip explosionSound; // 爆発SE
+    [SerializeField, Range(0f, 1f)] float explosionVolume = 1f; // 爆発音量
     public int b_damage = 5;
 
     private float b_explosion = 0;
@@ -47,6 +49,7 @@ public class Player_Bomb : MonoBehaviour
         b_pos = transform.position;
 
         PlayParticle();
+        PlayExplosionSound(); // SEを再生
         ApplyExplosionForce();
         // コールバック(プレイヤー側に爆発したことを知らせる)
         onExploded?.Invoke();
@@ -58,6 +61,16 @@ public class Player_Bomb : MonoBehaviour
     void PlayParticle()
     {
         Instantiate(explosion, b_pos, Quaternion.identity);
+    }
+
+    //SE再生
+    void PlayExplosionSound()
+    {
+        if (explosionSound != null)
+        {
+            // オブジェクトが破棄されても音が鳴るようにPlayClipAtPointを使用
+            AudioSource.PlayClipAtPoint(explosionSound, b_pos, explosionVolume);
+        }
     }
 
     //爆風
