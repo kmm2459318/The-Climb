1c8ffaf34445aed8b5c8ce40d9e2a14923822184
kmm-2459382 <kmm2459382@stu.o-hara.ac.jp>
2026-02-17 17:23:44 +0900

    ギミック作成
    ギミック作成

---
+++ The_Climb/Assets/Scenes/Umeda/Special/Script/SpikeDamage.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SpikeDamage : MonoBehaviour
+{
+    public int damage = 1; // 即死させたいなら maxHP 以上
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        PlayerHealth health = other.GetComponent<PlayerHealth>();
+        if (health != null)
+        {
+            health.TakeDamage(damage);
+        }
+    }
+}
