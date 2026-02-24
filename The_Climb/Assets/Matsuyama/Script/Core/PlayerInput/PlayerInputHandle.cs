b2a5e872b013b398428c80fff7ceb8a76d4b07c5
kmm-2459393 <kmm2459305@stu.o-hara.ac.jp>
2026-02-24 09:50:24 +0900

    最新状態にするためのプッシュ

---
+++ The_Climb/Assets/Matsuyama/Script/Core/PlayerInput/PlayerInputHandle.cs
@@ -0,0 +1,36 @@
+﻿//namespace TheClimb.Core
+//{
+//    public class PlayerInputHandle : InputHandleBase    //  プレイヤーの入力を受けつけるクラス
+//    {
+//        private InputSystem_Actions inputSystem_Action;
+
+//        bool IsClearAstral = false;
+
+//        public PlayerInputHandle(InputSystem_Actions inputSystem)
+//        {
+//            inputSystem_Action = inputSystem;
+
+//            SetReaction();
+//        }
+
+//        void SetReaction()
+//        {
+//            inputSystem_Action.Player.Enable();
+//            inputSystem_Action.Player.GenerateImpactBall.started += GenerateImpactBall;
+//        }
+
+//        void SetOffReaction()
+//        {
+//            inputSystem_Action.Player.GenerateImpactBall.started -= GenerateImpactBall;
+//            inputSystem_Action.Player.Disable();
+//        }
+//    }
+//}
+////if(PlayerPrefs.GetInt("Matsuyama") == 1)
+////{
+////    IsClearAstral = true;
+////}
+////else
+////{
+////    IsClearAstral = false;
+////}
