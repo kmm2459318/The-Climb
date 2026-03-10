2a2f6ff27dce79f8ee3e8a9551575811cfc99172
kmm2459378 <kmm2459378@stu.o-hara.ac.jp>
2026-03-10 09:52:17 +0900

    爆弾のSEが鳴るようにした

--- The_Climb/Assets/Script/Bomb/Player_Bomb.cs
+++ The_Climb/Assets/Script/Bomb/Player_Bomb.cs
@@ -1,20 +1,29 @@
 ﻿using UnityEngine;
-using UnityEngine.Rendering;
+using UnityEngine.Audio;
 
 public class Player_Bomb : MonoBehaviour
 {
+    [Header("Explosion")]
     [SerializeField] GameObject explosion;
-    [SerializeField] float b_force = 10;
-    [SerializeField] float b_radius = 5;
-    [SerializeField] float b_upward = 0;
-    [SerializeField] float b_time = 3;
+
+    [Header("Force")]
+    [SerializeField] float b_force = 10f;
+    [SerializeField] float b_radius = 5f;
+    [SerializeField] float b_upward = 0f;
+
+    [Header("Timer")]
+    [SerializeField] float b_time = 3f;
+
+    [Header("Sound")]
+    [SerializeField] AudioClip explosionSound;
+    [SerializeField] AudioMixerGroup seMixerGroup;
+
     public int b_damage = 5;
 
-    private float b_explosion = 0;
+    private float b_explosion = 0f;
     private bool exploded = false;
     private Vector3 b_pos;
 
-    // 爆発後に呼ばれるコールバック(Bomb_Generate側から登録)
     private System.Action onExploded;
 
     public void SetOnExplodedCallback(System.Action callback)
@@ -24,7 +33,7 @@ public class Player_Bomb : MonoBehaviour
 
     void Update()
     {
-        b_explosion += Time.deltaTime; 
+        b_explosion += Time.deltaTime;
 
         if (b_explosion >= b_time && !exploded)
         {
@@ -32,7 +41,6 @@ public class Player_Bomb : MonoBehaviour
         }
     }
 
-    // Bomb_Generateから呼ばれる強制爆発
     public void ForceExplosion()
     {
         if (!exploded)
@@ -47,69 +55,102 @@ public class Player_Bomb : MonoBehaviour
         b_pos = transform.position;
 
         PlayParticle();
+        PlayExplosionSound();
         ApplyExplosionForce();
-        // コールバック(プレイヤー側に爆発したことを知らせる)
+
         onExploded?.Invoke();
-        CameraShake.Instance.Shake(0.15f,0.11f);
-        Destroy(gameObject); //爆弾を削除
+
+        CameraShake.Instance.Shake(0.15f, 0.11f);
+
+        Destroy(gameObject);
     }
 
-    //パーティクル
     void PlayParticle()
     {
-        Instantiate(explosion, b_pos, Quaternion.identity);
+        if (explosion != null)
+        {
+            Instantiate(explosion, b_pos, Quaternion.identity);
+        }
     }
 
-    //爆風
+    void PlayExplosionSound()
+    {
+        if (explosionSound == null) return;
+
+        GameObject audioObj = new GameObject("ExplosionAudio");
+        audioObj.transform.position = b_pos;
+
+        AudioSource source = audioObj.AddComponent<AudioSource>();
+
+        source.clip = explosionSound;
+        source.spatialBlend = 0f;
+        source.outputAudioMixerGroup = seMixerGroup;
+
+        source.Play();
+
+        Destroy(audioObj, explosionSound.length);
+    }
+
     void ApplyExplosionForce()
     {
         Collider[] hitColliders = Physics.OverlapSphere(b_pos, b_radius);
-        
-        foreach(var hit in hitColliders)
-        {
-            var obj = hit.gameObject;
 
-            var rb = obj.GetComponent<Rigidbody>();
+        foreach (var hit in hitColliders)
+        {
+            GameObject obj = hit.gameObject;
+
+            Rigidbody rb = obj.GetComponent<Rigidbody>();
 
             if (rb == null) continue;
+
             rb.AddExplosionForce(b_force, b_pos, b_radius, b_upward, ForceMode.Impulse);
 
             ObjExplosionTarget(obj);
         }
     }
 
-    //破棄するobject
     void ObjExplosionTarget(GameObject obj)
     {
         switch (obj.tag)
         {
             case "BreakingWall":
+
                 Debug.Log("壁を発見");
-                DestructibleBlock Block = obj.GetComponent<DestructibleBlock>();
-                Block.BreakBlock();
+
+                DestructibleBlock block = obj.GetComponent<DestructibleBlock>();
+
+                if (block != null)
+                    block.BreakBlock();
+
                 break;
-        
+
             case "Enemy":
-                var enemy = obj.GetComponent<Enemy>();
+
+                Enemy enemy = obj.GetComponent<Enemy>();
+
                 if (enemy != null)
                 {
-                        enemy.TakeDamage(b_damage);
-                      Debug.Log("爆風ヒット");
+                    enemy.TakeDamage(b_damage);
+                    Debug.Log("爆風ヒット");
                 }
+
                 break;
 
             case "StalkerHand":
-                obj.GetComponent<KidnapBuddy>().handController.ReleaseBuddy();
+
+                obj.GetComponent<KidnapBuddy>()?.handController.ReleaseBuddy();
+
                 break;
 
             case "BossStalker":
-                var boss = obj.transform.parent.gameObject.GetComponent<BossStalkerHandController>();
-                boss.BossStalkerSlow();
-                break;
 
-            default: 
-            //何もしない
-            break;
+                BossStalkerHandController boss =
+                    obj.transform.parent.GetComponent<BossStalkerHandController>();
+
+                if (boss != null)
+                    boss.BossStalkerSlow();
+
+                break;
         }
     }
 }
